45b8a4d03b527f0ef0a639f9f9c73420fb156d4e
paolaandyco71 <paolaandyco71@gmail.com>
2017-10-24 21:09:54 -0300

    Inicio Proyecto

---
+++ src/proyecto1paola/Proyecto1paola.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1paola;
+
+/**
+ *
+ * @author LAB03
+ */
+public class Proyecto1paola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo, soy paola!!");
+    }
+    
+    
+}
